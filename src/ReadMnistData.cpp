320dd81c0555b7d10308b2351ce6e497e50af938
Manikandan S <160469597+smani2024@users.noreply.github.com>
2024-03-07 08:44:44 +0530

    Add files via upload

---
+++ src/ReadMnistData.cpp
@@ -0,0 +1,82 @@
+#include "ReadMnistData.h"
+
+namespace mnist {
+
+std::vector<std::vector<unsigned char>> ReadMnistData::readIDX3UByteFile(const std::string& filename) {
+
+    std::ifstream file(filename, std::ios::binary);
+
+    if (!file) {
+        std::cerr << "Failed to open the IDX3-UBYTE file." << std::endl;
+        return {};
+    }
+
+    // Read the IDX3-UBYTE file header
+    char magicNumber[4];
+    char numImagesBytes[4];
+    char numRowsBytes[4];
+    char numColsBytes[4];
+
+    file.read(magicNumber, 4);
+    file.read(numImagesBytes, 4);
+    file.read(numRowsBytes, 4);
+    file.read(numColsBytes, 4);
+    std::cout<<static_cast<int>(numImagesBytes[0])<<"  "<<static_cast<int>(numImagesBytes[1])<<"  "<<
+        (int)static_cast<unsigned char>(numImagesBytes[2])<<"  "<<static_cast<int>(numImagesBytes[3])<<"  "<<std::endl;
+
+    // Convert the header information from big-endian to native endianness
+    int numImages = (static_cast<unsigned char>(numImagesBytes[0]) << 24) | (static_cast<unsigned char>(numImagesBytes[1]) << 16) | (static_cast<unsigned char>(numImagesBytes[2]) << 8) | static_cast<unsigned char>(numImagesBytes[3]);
+    int numRows = (static_cast<unsigned char>(numRowsBytes[0]) << 24) | (static_cast<unsigned char>(numRowsBytes[1]) << 16) | (static_cast<unsigned char>(numRowsBytes[2]) << 8) | static_cast<unsigned char>(numRowsBytes[3]);
+    int numCols = (static_cast<unsigned char>(numColsBytes[0]) << 24) | (static_cast<unsigned char>(numColsBytes[1]) << 16) | (static_cast<unsigned char>(numColsBytes[2]) << 8) | static_cast<unsigned char>(numColsBytes[3]);
+
+    // Initialize a vector to store the images
+    std::vector<std::vector<unsigned char>> images;
+
+    for (int i = 0; i < numImages; i++) {
+        // Read each image as a vector of bytes
+        std::vector<unsigned char> image(numRows * numCols);
+        file.read((char*)(image.data()), numRows * numCols);
+
+        images.push_back(image);
+    }
+
+    file.close();
+
+    return images;
+}
+
+std::vector<std::vector<unsigned char>> ReadMnistData::readLabelFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::binary);
+
+    if (!file) {
+        std::cerr << "Failed to open the IDX3-UBYTE file." << std::endl;
+        return {};
+    }
+
+    // Read the IDX3-UBYTE file header
+    char magicNumber[4];
+    char numImagesBytes[4];
+
+    file.read(magicNumber, 4);
+    file.read(numImagesBytes, 4);
+
+    // Convert the header information from big-endian to native endianness
+    int numImages = (static_cast<unsigned char>(numImagesBytes[0]) << 24) | (static_cast<unsigned char>(numImagesBytes[1]) << 16) | (static_cast<unsigned char>(numImagesBytes[2]) << 8) | static_cast<unsigned char>(numImagesBytes[3]);
+
+    // Initialize a vector to store the images
+    std::vector<std::vector<unsigned char>> images;
+
+    for (int i = 0; i < numImages; i++) {
+        // Read each image as a vector of bytes
+        std::vector<unsigned char> image(1);
+        file.read((char*)(image.data()), 1);
+
+        images.push_back(image);
+    }
+
+    file.close();
+
+    return images;
+    }
+
+}
